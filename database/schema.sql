05b61f604492ca22dd31ebf172f98f3fc655a0df
keivanmojmali <mojmalik@gmail.com>
2021-02-25 14:56:15 -0800

    schema fixed

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,32 @@
-#!/bin/bash -e
+set client_min_messages to warning;
 
-test -f .env && source .env
-psql $DATABASE_URL -f database/schema.sql -f database/data.sql
+-- DANGER: this is NOT how to do it in the real world.
+-- `drop schema` INSTANTLY ERASES EVERYTHING.
+drop schema "public" cascade;
+create schema "public";
+
+CREATE TABLE "products" (
+	"productId" serial NOT NULL,
+	"name" VARCHAR(255) NOT NULL,
+	"price" VARCHAR(255) NOT NULL,
+	"color" VARCHAR(255) NOT NULL,
+	"description" VARCHAR(255) NOT NULL,
+	"image" VARCHAR(255) NOT NULL,
+	CONSTRAINT "products_pk" PRIMARY KEY ("productId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "users" (
+	"userId" serial NOT NULL,
+	"firstName" VARCHAR(255) NOT NULL,
+	"lastName" VARCHAR(255) NOT NULL,
+	"email" VARCHAR(255) NOT NULL,
+	"image" VARCHAR(255) NOT NULL,
+	"password" VARCHAR(255) NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
