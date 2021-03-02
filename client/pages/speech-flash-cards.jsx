d795157d9d11d4a19db215d9640dca4b1d6a1fac
keivanmojmali <mojmalik@gmail.com>
2021-03-01 17:53:45 -0800

    working on function

--- client/pages/speech-flash-cards.jsx
+++ client/pages/speech-flash-cards.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { sources } from 'webpack';
 
 export default class SpeechFlashCards extends React.Component{
   constructor(props){
@@ -11,7 +12,6 @@ export default class SpeechFlashCards extends React.Component{
     this.checkWord = this.checkWord.bind(this)
     this.currentCard = this.currentCard.bind(this)
     this.dictate = this.dictate.bind(this);
-    this.microphone = document.querySelector('.fas fa-microphone');
     window.SpeechRecognition = webkitSpeechRecognition || window.SpeechRecognition;
     this.recognition = new SpeechRecognition();
     this.recognition.interimResults = true;
@@ -22,26 +22,21 @@ export default class SpeechFlashCards extends React.Component{
     .then(result=>result.json())
     .then(data=>this.setState({words:data}))
     .catch(err=>console.error(err))
-
-    this.microphone.addEventListener('click', () => {
-      //Add sound play here
-      this.dictate();
-
-    })
   }
-  dictate() {
+  dictate(word) {
     this.recognition.start();
     this.recognition.onresult = (event) => {
     const speechToText = Array.from(event.results)
     .map(result =>result[0])
     .map(result => result.transcript)
     .join(' ');
-    this.checkWord(speechToText);
+    this.checkWord(word,speechToText);
     }
   }
-  checkWord(speechToText) {
-    if (speechToText === this.state.currentWord) {
-      this.setState({pass:true})
+  checkWord(word,speechToText) {
+    console.log(word,speechToText)
+    if (word === speechToText) {
+      console.log('yah!')
     } else {
       console.log('no!')
     }
@@ -58,7 +53,7 @@ export default class SpeechFlashCards extends React.Component{
               <div className="card-body d-flex flex-column align-items-center">
                 <h5 className="card-title text-center display-2">{item.word}</h5>
                 <p className="card-text text-center">Press the microphone button below and speak the word above.</p>
-                <button className='fas fa-microphone' id={item.wordId}></button>
+                <button className='fas fa-microphone' onClick={()=>{this.dictate(item.word)}}></button>
               </div>
             </div>
           </div>
