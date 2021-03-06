c325c1b3d7f7eaf8763e598beb1c9c0e858b2097
keivanmojmali <mojmalik@gmail.com>
2021-03-05 16:58:15 -0800

    working

--- client/pages/press-sight-word.jsx
+++ client/pages/press-sight-word.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 
-export default class PressSightWord {
+export default class PressSightWord extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-
+      words: [],
     };
-
+    this.currentSentence = this.currentSentence.bind(this);
+  }
+  componentDidMount(){
+    fetch('/store/getWords')
+      .then(result => result.json())
+      .then(data => this.setState({ words: data }))
+      .catch(err => console.error(err))
+  }
+  currentSentence(){
+    return(
+      <>
+      <div className='row'>
+        <div className="col">
+          <h1>Press the Sight Word Below</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          {this.currentSentence()}
+        </div>
+      </div>
+      </>
+    )
   }
   render(){
     return(
-      <div>
-        <h1>Hello</h1>
+      <div className='col'>
+        {this.currentSentence()}
       </div>
     )
   }
