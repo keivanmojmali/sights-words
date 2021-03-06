e811f758c4ddf6b848bc115cd89c6be3c8cc4b1d
keivanmojmali <mojmalik@gmail.com>
2021-03-05 17:08:17 -0800

    working on it

--- client/pages/press-sight-word.jsx
+++ client/pages/press-sight-word.jsx
@@ -4,25 +4,29 @@ export default class PressSightWord extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      sentences: [],
+      sentences: [
+        {
+          word: the,
+          sentences: [Please, get, the, door],
+        }
+      ],
+      currentIndex: 0,
     };
     this.currentSentence = this.currentSentence.bind(this);
     this.currentWord = this.currentWord.bind(this);
   }
   componentDidMount(){
-    fetch('/store/getWords')
-      .then(result => result.json())
-      .then(data => this.setState({ words: data }))
-      .catch(err => console.error(err))
+    // ADD THE FETCH FOR THE SENTENCES HERE
   }
   currentSentence(sentenceArray){
     return sentenceArray.map((item,index)=>{
       return (
-        <button className='btn'>{sentenceArray.word}</button>
+        <button onClick={this.handleClick()} className='btn'>{sentenceArray.sentence}</button>
       )
     })
   }
   currentWord(){
+    const currentSentence = this.state.sentences[this.state.currentIndex];
     return(
       <>
       <div className='row'>
